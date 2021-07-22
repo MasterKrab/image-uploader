6714aadef3dd4a4d0fffe43f547276d9e0d37d45
Enzo <enzovivallo05@outlook.com>
2021-07-21 23:24:54 -0400

    fix cors backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/public`));
 
+// Cors
+const cors = require("cors");
+
+const corsConfig = {
+  origin: "https://image-uploader-orcin.vercel.app/",
+};
+
+app.use(cors(corsConfig));
+
 // Routes
 app.use("/", require("./router/images"));
 
